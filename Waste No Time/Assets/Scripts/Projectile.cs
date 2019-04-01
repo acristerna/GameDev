096612754f91a66985439f1c4f589b796379ac36
acristerna <48137386+acristerna@users.noreply.github.com>
2019-03-31 20:47:38 -0500

    Attack Level Shooting
    Added Attack Level Shooting
    Cursor triggered gun
    Weapon Script
    Enemy  Script
    Projectile Script

---
+++ Waste No Time/Assets/Scripts/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public float distance;
+    public int damage;
+    public LayerMask whatIsSolid;
+    
+
+
+    public GameObject destroyEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifetime);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance, whatIsSolid);
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("Enemy MUST TAKE DAMAGE!");
+                hitInfo.collider.GetComponent<Enemy>().TakeDamage(damage);
+            }
+
+            DestroyProjectile();
+        }
+    
+
+    transform.Translate(Vector2.right* speed * Time.deltaTime);
+}
+
+    void DestroyProjectile()
+    {
+       Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
