c706f7114bf1848eee54e65341c29c1ad3761aa3
acristerna <48137386+acristerna@users.noreply.github.com>
2019-04-05 20:18:07 -0500

    Boss Level Updates
    Boss follows and retracts from character. Also shoots object to character position.
    
    no current animation added yet

--- Waste No Time/Assets/Scripts/Enemy.cs
+++ Waste No Time/Assets/Scripts/Enemy.cs
@@ -1,11 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public int health;
     public GameObject deathEffect;
+
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    public GameObject projectile;
+    
+
+    public Transform player;
+
+    public Slider healthBar;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBtwShots = startTimeBtwShots;
+    }
 
     private void Update()
     {
@@ -14,6 +35,31 @@ public class Enemy : MonoBehaviour
             Instantiate(deathEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
+
+        if (Vector2.Distance(transform.position,player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if(timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+
+        healthBar.value = health;
     }
 
     public void TakeDamage(int damage)
@@ -23,3 +69,4 @@ public class Enemy : MonoBehaviour
     }
 
 }
+
