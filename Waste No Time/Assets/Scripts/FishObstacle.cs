c50f723c241c82081bd29983566435f90e0da489
nhishon <nhishon@luc.edu>
2019-05-03 09:22:17 -0400

    water level updates

---
+++ Waste No Time/Assets/Scripts/FishObstacle.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishObstacle : MonoBehaviour
+{
+
+    public int damage = 1;
+    [SerializeField] int health = 100;
+    [SerializeField] int scoreValue = 1;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject != null)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // player takes damage !
+            other.GetComponent<Player>().health -= damage;
+            //Debug.Log(other.GetComponent<Player>().health);
+            Destroy(gameObject);
+        }
+        else
+        {
+
+            DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+            health -= damageDealer.GetDamage();
+            FindObjectOfType<GameSession>().AddToScore(scoreValue);
+            if (health <= 0)
+            {
+
+                Destroy(gameObject);
+            }
+            Destroy(other.gameObject);
+
+        }
+
+
+    }
+}
---
+++ Waste No Time/Assets/Scripts/FishSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour
+{
+    public GameObject[] obstaclePatterns;
+    private float timeBwtSpawn;
+    public float startTimeBwtSpawn;
+    public float decreaseTime;
+    public float minTime = 0.65f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBwtSpawn <= 0)
+        {
+            int rand = Random.Range(0, obstaclePatterns.Length);
+            Debug.Log(rand);
+            Instantiate(obstaclePatterns[rand], transform.position, Quaternion.identity);
+            timeBwtSpawn = startTimeBwtSpawn;
+            if (startTimeBwtSpawn > minTime)
+            {
+                startTimeBwtSpawn -= decreaseTime;
+            }
+        }
+        else
+        {
+            timeBwtSpawn -= Time.deltaTime; 
+        }
+    }
+}
